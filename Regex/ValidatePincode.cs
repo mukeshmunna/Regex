b020f2f216bc6e4ce1e737288881d87c0622ba3e
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-02 12:02:07 +0530

    [Mukesh] Add: To Validate the UC4 Pincode

--- Regex/ValidatePincode.cs
+++ Regex/ValidatePincode.cs
@@ -36,7 +36,7 @@ namespace RegexProblems
         }
         public void UC4_Validate(string input)
         {
-            bool result = Regex.IsMatch(input, uc4_Regex);
+            bool result = Regex.IsMatch(input, uc1_Regex);
             if (result)
                 Console.WriteLine("Validation Successful");
             else
