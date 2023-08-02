91cfcd31d9b44b870b9fc8ae1ac0ecf195973696
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-02 11:41:45 +0530

    [Mukesh] Add: Validate the Pincode

---
+++ Regex/Program.cs
@@ -0,0 +1,12 @@
+﻿using RegexProblems;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Regex Functionality");
+        ValidatePincode validatePincode = new ValidatePincode();
+        validatePincode.Validate("601201");
+
+    }
+}
